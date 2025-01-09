4d09df52d4335f8bf11b3aac54d256166d1d61fb
student-02-b1edbc847490 <kambletanaya18@gmail.com>
2025-01-09 09:28:03 -0800

    host

--- brain/settings.py
+++ brain/settings.py
@@ -35,7 +35,7 @@ DEBUG = env('DEBUG', default=False)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["app.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 #Email settings
 EMAIL_HOST="smtp.gmail.com" #smtp=simple mail transfer protocol
